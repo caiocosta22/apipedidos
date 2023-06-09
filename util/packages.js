1c94363806aa1a8daa72f215b72b942c6ec16ae7
caiocosta22 <caiocosta22@gmail.com>
2023-06-09 13:04:28 -0300

    adição do arquivo package

--- util/packages.js
+++ util/packages.js
@@ -1,5 +1,6 @@
 //Arquivo de comentário para saber quais pacotes e bibliotecas utilizei durante o projeto
-//NPM para instalar os pacotes
+//NPM init para instalar os pacotes
+//Debug para debugar
 //Yarn para biblioteca de testes de codigo (substituto do nodemon)
 //Express como biblioteca responsável por estabelecer os padrões REST
 //HTTP para estabelecer a conexão HTTP com as rotas da API
