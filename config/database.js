28a7b840483098927c1a90136e27c3e82574f39c
caiocosta22 <caiocosta22@gmail.com>
2023-06-12 09:30:08 -0300

    Conexão com POSTGREE sucedida

--- config/database.js
+++ config/database.js
@@ -17,14 +17,45 @@ const sqlconfig = {
 };
 
 // Teste de conexão com o banco de dados
-async function testConnection() {
+async function Connectionsql() {
   try {
     await sql.connect(sqlconfig);
-    console.log("Conexão com banco de dados sucedida!");
+    console.log("Conexão com banco de dados SQL sucedida!");
   } catch (err) {
-    console.log("Falha na conexão com o banco de dados");
+    console.log("Falha na conexão com o banco de dados SQL");
     throw err;
   }
   sql.close();
 };
-testConnection();
+Connectionsql();
+
+// Conexão com o PG
+
+const { Client } = require('pg');
+
+const pgconfig = new Client({
+  user: 'gprod',
+  host: 'CLOUD64.P80.COM.BR',
+  database: 'gpinformatica2',
+  password: 'g2@9876@g2',
+  port: '5432',
+});
+
+// Teste de conexão com o banco de dados
+async function Connectionpg() {
+  try {
+    await pgconfig.connect();
+    console.log("Conexão com banco de dados POSTGREE sucedida!");
+      
+      const query = 'SELECT * FROM SITE_PEDIDOS_FATURADOS LIMIT 1';
+      const result = await pgconfig.query(query);
+      console.log("Tentativa de consulta:   ",result.rows);
+
+  } catch (err) {
+    console.log("Falha na conexão com o banco de dados POSTGREE");
+    throw err;
+  } finally {
+  await pgconfig.end();
+  }
+};
+Connectionpg();
