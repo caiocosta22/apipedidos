7385521de7c0be6b291e95e43a8c7c5150df6778
caiocosta22 <caiocosta22@gmail.com>
2023-06-13 17:11:32 -0300

    Conexão e consulta com Postgre realizadas.

--- config/database.js
+++ config/database.js
@@ -1,5 +1,5 @@
-'use strict'
-const pg = require('pg')
+'use strict';
+
 const sql = require('mssql');
 // Conexão com o SQL
 const sqlconfig = {
@@ -29,35 +29,7 @@ const sqlconfig = {
 };
 Connectionsql();
 */
-// Conexão com o PG
-
-const { Client } = require('pg');
-const pgconfig = new Client({
-  user: 'gprod',
-  host: 'CLOUD64.P80.COM.BR',
-  database: 'gpinformatica2',
-  password: 'g2@9876@g2',
-  port: '5432',
-});
-
-// Teste de conexão com o banco de dados
-/*async function Connectionpg() {
-  try {
-    await pgconfig.connect();
-    console.log("Conexão com banco de dados POSTGREE sucedida!");
-      
-      const query = 'SELECT * FROM SITE_PEDIDOS_FATURADOS LIMIT 1';
-      const result = await pgconfig.query(query);
-      console.log("Tentativa de consulta:   ",result.rows);
-
-  } catch (err) {
-    console.log("Falha na conexão com o banco de dados POSTGREE");
-    throw err;
-  } finally {
-  await pgconfig.end();
-  }
-};
-Connectionpg();*/
 
 
-module.exports = sqlconfig, pgconfig
+
+module.exports = sqlconfig
