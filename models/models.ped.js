76bc5c2d5a532652ca40a775982f97433e7ddb97
caiocosta22 <caiocosta22@gmail.com>
2023-06-15 16:19:09 -0300

    Tentativa de utilizar a função foreach

--- models/models.ped.js
+++ models/models.ped.js
@@ -6,19 +6,19 @@ const sqlpool = require ('../config/sqlconfig.js');
 async function checapedidosql() {
     try {
       // Conexão com o SQL
-      await sqlpool.connect();
-      console.log('Conexão bem-sucedida com o SQL SERVER');
+        await sqlpool.connect();
+        console.log('Conexão bem-sucedida com o SQL SERVER');
   
       // Introdução da Consulta
-      let query = `SELECT TOP 1 * FROM CONSULTA_CEP`;
-      const result = await sqlpool.request().query(query);
-      const resultadosql = result.recordset
-      console.log(resultadosql);
+        let query = `SELECT TOP 1 * FROM CONSULTA_CEP`;
+        const result = await sqlpool.request().query(query);
+        const resultadosql = result.recordset
+        console.log(resultadosql);
   
       // Encerrar conexão
-      sqlpool.close();
+        sqlpool.close();
     } catch (err) {
-      console.error('Erro ao executar a consulta: ', err);
+        console.error('Erro ao executar a consulta: ', err);
     };
 };
 
@@ -38,36 +38,37 @@ async function checapedidopg() {
         throw console.log(error);
     } finally {
         await pgpool.release();
-    }
+    };
 };
 
 async function inserepedidosql(cep) {
     try {
     // Inicio da conexão com o banco
-    await sqlpool.connect();
-    console.log("Conexão com o banco de dados SQL sucedida!");
+        await sqlpool.connect();
+        console.log("Conexão com o banco de dados SQL sucedida!");
 
     // Verificar se o cep já existe no banco 
-    let query = `SELECT CEP FROM dbo.CONSULTA_CEP WHERE cep = '${cep}'`
-    const result = await sqlpool.query(query, [cep]);
+        let query = `SELECT CEP FROM dbo.CONSULTA_CEP WHERE cep = '${cep}'`
+        const result = await sqlpool.query(query, [cep]);
     
-    if (result.recordset.length > 0) {
-        console.log("O cep já existe no banco de dados");
-        return;
-      }
-    else {
+        if (result.recordset.length > 0) {
+            console.log("O cep já existe no banco de dados");
+            return;
+        } else {
+
     // Modelagem da inserção
-        let insertsql = `INSERT INTO dbo.CONSULTA_CEP(cep, logradouro, complemento, bairro, localidade, uf, ibge, gia, ddd, siafi)`;
-        insertsql += `VALUES('${cep}','Praça teste','Complemento teste','Bairro teste','Localidade','CE','230440','','85','1389')`;
+            let insertsql = `INSERT INTO dbo.CONSULTA_CEP(cep, logradouro, complemento, bairro, localidade, uf, ibge, gia, ddd, siafi)`;
+            insertsql += `VALUES('${cep}','Praça teste','Complemento teste','Bairro teste','Localidade','CE','230440','','85','1389')`;
 
     // Tentativa de execução
-        await sqlpool.query(insertsql, [cep]);
-        console.log("Dados inseridos com suceso")
+            await sqlpool.query(insertsql, [cep]);
+            console.log("Dados inseridos com suceso");
+
     // Encerrar conexão
-        sqlpool.close();
-    };
+            sqlpool.close();
+        };
     } catch (err) {
-    console.error('Erro com o processo ', err);
+        console.error('Erro com o processo ', err);
     };
 };
 
@@ -78,30 +79,45 @@ async function testefinal(){
         console.log("Conexão com o banco de dados SQL sucedida!");
     
     //Consulta SQL
-        let ssql = `SELECT CONTA FROM dbo.V_SITE_PEDIDOS_FATURADOS`;
+        let ssql = `SELECT * FROM dbo.v_siteMovimento_Dia`;
         const resultsql = await sqlpool.request().query(ssql);
+        const contasSql = resultsql.recordset.map(row => row.CONTA);
         console.log(resultsql);
 
-    // Inicio da conexão com POSTGREE
+    // Inicio da conexão com POSTGRE
         await pgpool.connect();
-        console.log("Conexão com banco de dados POSTGREE sucedida!");
+        console.log("Conexão com banco de dados POSTGRE sucedida!");
     
     // Consulta Postgree
-        const query = `SELECT CONTA FROM SITE_PEDIDOS_FATURADOS WHERE IDG2 = 3632`;
+        const query = `SELECT CONTA FROM SITE_MOVIMENTO_DIA WHERE IDG2 = 3353`;
         const resultpg = await pgpool.query(query);
-        console.log(resultpg.rows);
+        const registropg = resultpg.rows;
+        console.log(registropg);
 
-    // Tentativa de Comparação
-        if (resultsql<resultpg.rows){
-            console.log("Postgree é maior")
-            try{
-                
-            } catch{
+    // Filtrando o banco
+        const contasFaltantes = registropg.filter(row => !contasSql.includes(row.CONTA));
+        console.log("Contas faltantes ", contasFaltantes);
 
-            };
+    // Pegar todas as variaveis do postegress que salvamos no jsonArray e guardar no newJsonArray, pois antes tinhamos apenas a informacao do cep
+        registropg.forEach(row => {
+            contasSql.forEach(conta => {
+            if(conta === registropg) {
+                contasFaltantes.push(registropg)
+            }});
+        });
+
+    // Inserção no SQL 
+        if (contasFaltantes.length > 0) {
+            
+            const valores = contasFaltantes.map(row => `('${row.IDG2}','${row.CONTA}', '${row.ENTIDADEID_LOJA}', '${row.ALMOXID}', '${row.NUMDOCUMENTO}'${row.STATUS}', '${row.TIPO}', '${row.DATAEMISSAO}', '${row.ENTIDADEID_CLIENTE}', '${row.ENTIDADEID_FUNC}', '${row.DESCONTO}', '${row.VALORTOTALPROD}', '${row.VALORTOTALNOTA}', '${row.VALDESCONTO}', '${row.TIPOSERVID}','${row.PEDCLIENTE}','${row.CONDICAOID}','${row.FORMAPAGID}','${row.DATAFECHAMENTO}','${row.STATUS_CONF}','${row.ENTIDADEID_PARCEIRO}','${row.ENTIDADEID_FUNC2}',)`).join(',');
+            const inserirdados = `INSERT INTO dbo.v_siteMovimento_Dia (IDG2, CONTA, entidadeid_loja, ALMOXID, NUMDOCUMENTO, STATUS, TIPO, DATAEMISSAO, ENTIDADEID_CLIENTE, ENTIDADEID_FUNC, DESCONTO, VALORTOTALPROD, VALORTOTALNOTA, VALDESCONTO, TIPOSERVID, PEDCLIENTE, CONDICAOID, FORMAPAGID, DATAFECHAMENTO, STATUS_CONF, ENTIDADEID_PARCEIRO, ENTIDADEID_FUNC2) VALUES ${valores}`;
+            
+            await sqlpool.request().query(inserirdados);
+            console.log("Bancos sincronizados");
         } else {
-            return console.log("Bancos sicronizados")
+            console.log("Dados já estão atualizados");
         };
+
     // Capturando erro
     } catch (err){
         console.error('Erro com o processo ', err);
