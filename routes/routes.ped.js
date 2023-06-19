4a4cea6ff1f3c33456032ec23c9a21de09f11653
caiocosta22 <caiocosta22@gmail.com>
2023-06-19 09:49:37 -0300

    Sincronizando itens e pedidos - OK

--- routes/routes.ped.js
+++ routes/routes.ped.js
@@ -8,9 +8,3 @@ router.post('/sincronizar', controllers.sincronizar);
 
 module.exports = router;
 
-// http error 200: concluido com sucesso
-// http error 201: criado com sucesso
-// http error 400: bad request (requisição com alguma falha)
-// http error 401: autenticação inválida
-// http error 403: acesso negado
-// http error 500: internal server error (qualquer error que der na aplicação, vai ser retornado esse)
